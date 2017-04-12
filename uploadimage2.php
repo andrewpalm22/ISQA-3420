fa7ddbf2baf5fe8f3c09d8cf4c2ace809b8dc5e2
apalmesano2 <andrewpalm22@gmail.com>
2017-04-12 11:30:41 -0500

    Update uploadimage2.php

--- uploadimage2.php
+++ uploadimage2.php
@@ -1,6 +1,8 @@
-<?php $target_dir = "policy/"; $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1; $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION); // Check if image file is a
-actual image or fake image if(isset($_POST["submit"])) {
+<?php $target_dir = "policy/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1; $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+ // Check if image file is a actual image or fake image
+ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
@@ -10,20 +12,23 @@ actual image or fake image if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-// Check if file already exists if (file_exists($target_file)) {
+// Check if file already exists
+ if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-// Check file size if ($_FILES["fileToUpload"]["size"] > 500000) {
+// Check file size
+ if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-// Allow certain file formats if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType !=
-"jpeg" && $imageFileType != "gif" ) {
+// Allow certain file formats
+ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "JPG" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error if ($uploadOk == 0) {
+// Check if $uploadOk is set to 0 by an error
+ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded."; // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
